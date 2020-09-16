44a2a23b848a4a3b68edf15bc49ef070c9a534c5
Marcus Vinicius Holanda de Lima <marcus.vinicius.99@hotmail.com>
2020-09-15 21:23:53 -0300

    Update PID, needs testing

--- controller/PID.py
+++ controller/PID.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class PID(object):
     def __init__(self, kp, kd ,ki):
@@ -32,17 +33,44 @@ class PID(object):
 
 class Robot_PID(object):
 
-    def __init__(self, linear_pid, angular_pid):
-        self.linear_pid = linear_pid
-        self.angular_pid = angular_pid
+    def __init__(self, robot):
+        self.robot = robot
+        self.game = self.robot.game
 
-    def update_Speed(self, linear_desired, angular_desired, now_linear, now_angular, fps):
+        self.desired = np.array([0,0])
+
+        self.linear_pid = PID(1,0,0)
+        self.angular_pid = PID(1,0,0)
+
+    def update(self):
+        linear_speed, angular_speed = self.robot._get_differential_robot_speeds(self.robot.vx, self.robot.vy, self.robot.theta)
+        linear_desired, angular_desired = self.robot._get_desired_differential_robot_speeds(self.desired[0],self.desired[1], self.robot.theta)
+        vl, va = self.update_Speed(linear_desired,angular_desired,linear_speed, angular_speed)
+
+        acc_left  = vl + va
+        acc_right = vl - vl
+
+        power_left = power_left + acc_left * (1/self.game._fps)
+        power_right = power_right + acc_right * (1/self.game._fps)
+
+        return power_left , power_right
+
+
+
+    def set_desired(self, vector):
+        self.desired = vector
+
+
+
+
+    def update_Speed(self, linear_desired, angular_desired, now_linear, now_angular):
         self.linear_desired = linear_desired
         self.angular_desired = angular_desired
 
         self.linear_pid.set_desired_PID(linear_desired)
         self.angular_pid.set_desired_PID(angular_desired)
 
-        self.linear_pid.update_PID(now_linear,fps)
-        self.angular_pid.update_PID(now_angular,fps)
+        vl = self.linear_pid.update_PID(now_linear,self.game._fps)
+        va = self.angular_pid.update_PID(now_angular,self.game._fps)
 
+        return vl, va
