9437d1d625d89c5b7a3eadaf4ef434f97b03135b
Alexsandro Santos <alexsandro2134@gmail.com>
2021-07-06 22:26:13 -0300

    Add Coach ABC

---
+++ entities/coach/coach.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class BaseCoach(ABC):
+    NAME = "BASE_COACH"
+    def __init__(self, match):
+        self.match = match
+
+    @abstractmethod
+    def decide (self):
+        raise NotImplementedError("Coach needs decide implementation!")
+
+    def get_positions(self, foul, team_color):
+        return None
