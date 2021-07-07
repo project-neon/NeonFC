3683d6d1d13ac487d80063f20f51ad3ec8b44850
gTorrez <g.ctorres09@gmail.com>
2021-07-07 18:54:58 -0300

    add some test for the new improvements

--- entities/coach/iron2021.py
+++ entities/coach/iron2021.py
@@ -14,18 +14,26 @@ class Coach(object):
 
         one_to_one_play = plays.OneOneOnePlay(self)
         unstuck_play = plays.UnstuckPlay(self)
+        free_ball_play = plays.FreeBallPlay(self)
 
         stuck_trigger = plays.StuckRobotsTrigger(stuck_strategies=['attacker', 'midfielder'])
         wait_for_trigger = plays.WaitForTrigger(5)
+        free_ball_trigger = plays.OnFreeBall(self.match.game.referee)
 
         one_to_one_play.add_transition(stuck_trigger, unstuck_play)
         unstuck_play.add_transition(wait_for_trigger, one_to_one_play)
+        one_to_one_play.add_transition(free_ball_trigger, free_ball_play)
+        free_ball_play.add_transition(wait_for_trigger, one_to_one_play)
 
         self.playbook.add_play(one_to_one_play)
         self.playbook.add_play(unstuck_play)
+        self.playbook.add_play(free_ball_play)
 
         self.playbook.set_play(one_to_one_play)
     
+    def get_positions(self, foul, team_color):
+        return None
+
     def decide (self):
         self.playbook.update()
 
--- entities/plays/__init__.py
+++ entities/plays/__init__.py
@@ -2,6 +2,13 @@ from entities.plays.playbook import Playbook
 
 from entities.plays.playbook import OneOneOnePlay
 from entities.plays.playbook import UnstuckPlay
+from entities.plays.playbook import FreeBallPlay
 
 from entities.plays.playbook import StuckRobots as StuckRobotsTrigger
 from entities.plays.playbook import WaitFor as WaitForTrigger
+
+from entities.plays.playbook import OnFreeBall
+from entities.plays.playbook import OnFreeKick
+from entities.plays.playbook import OnKickOff
+from entities.plays.playbook import OnGoalKick
+from entities.plays.playbook import OnPenaltyKick
