85b33298d7e5f3d5dcf8b5ca2c9f204e3f6880ee
gTorrez <g.ctorres09@gmail.com>
2021-07-11 15:02:45 -0300

    deleted FreeBallPlay (test)

--- entities/plays/playbook.py
+++ entities/plays/playbook.py
@@ -169,57 +169,6 @@ class UnstuckPlay(Play):
     def _elect_midfielder(self, robot):
         return 1
 
-class FreeBallPlay(Play): # teste
-    def __init__(self, coach):
-        super().__init__(coach)
-        self.match = self.coach.match
-        self.constraints = [
-            #estratégia - função eleitora - robot_id
-            (strategy.larc2020.GoalKeeper(self.match), self._elect_goalkeeper, 0),
-            (strategy.larc2020.Attacker(self.match), self._elect_attacker, 0),
-            (strategy.larc2020.MidFielder(self.match), self._elect_midfielder, 0)
-        ]
-
-    def update(self):
-        super().update()
-
-        robots = [r.robot_id for r in self.match.robots]
-
-        for strategy, fit_fuction, priority in self.constraints:
-            elected = -1
-            best_fit = -99999
-            for robot_id in robots:
-                robot_fit = fit_fuction(self.match.robots[robot_id])
-                if (robot_fit > best_fit):
-                    best_fit = robot_fit
-                    elected = robot_id
-
-            priority = elected
-            if self.match.robots[elected].strategy is None:
-                self.match.robots[elected].strategy = strategy
-            elif self.match.robots[elected].strategy.name != strategy.name:
-                self.match.robots[elected].strategy = strategy
-                self.match.robots[elected].start()
-            robots.remove(elected)
-
-    def _elect_attacker(self, robot):
-
-        is_behind = 2 if robot.x > self.match.ball.x else 1
-
-        dist_to_ball = math.sqrt(
-            (robot.x - self.match.ball.x)**2 + (robot.y - self.match.ball.y)**2
-        )
-        return 1000 - dist_to_ball * is_behind
-
-    def _elect_goalkeeper(self, robot):
-        dist_to_goal = math.sqrt(
-            (robot.x - 0)**2 + (robot.y - 0.65)**2
-        )
-        return 1000 - dist_to_goal
-
-    def _elect_midfielder(self, robot):
-        return 1  
-
 class WaitFor(Trigger):
     def __init__(self, timeout):
         """
