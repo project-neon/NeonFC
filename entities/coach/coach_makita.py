c3ee0ea538a752f65687a9e7df5037e605d4ca8d
Eduardo Makita <makitayukio1@gmail.com>
2021-08-25 19:42:22 -0300

    add restrictive and ball-following potential fields

--- entities/coach/coach_makita.py
+++ entities/coach/coach_makita.py
@@ -12,6 +12,13 @@ class Coach(BaseCoach):
         self.goalkeeper_strategy = strategy.tests.mktGoalKeeper(self.match)
 
     def decide(self):
+        self.match.robots[0].strategy = self.goalkeeper_strategy
+        self.match.robots[0].start()
+        self.match.robots[1].strategy = self.attacker_strategy
+        self.match.robots[1].start()
+        self.match.robots[2].strategy = self.midfielder_strategy
+        self.match.robots[2].start()
+        '''
         robots = [r.robot_id for r in self.match.robots]
         strategies = {
             self.goalkeeper_strategy, self.attacker_strategy, self.midfielder_strategy
@@ -23,4 +30,4 @@ class Coach(BaseCoach):
                 continue
         
             self.match.robots[robot].strategy = strategy
-            self.match.robots[robot].start()
+            self.match.robots[robot].start()'''
--- strategy/tests/mktGoalKeeper.py
+++ strategy/tests/mktGoalKeeper.py
@@ -5,15 +5,23 @@ import controller
 from strategy.BaseStrategy import Strategy
 from strategy.DebugTools import DebugPotentialFieldStrategy
 
+'''
+esquerda: theta = 0
+direita: theta = math.pi
+'''
+
 class mktGoalKeeper(Strategy):
-    def __init__(self, match, plot_field=False):
-        super().__init__(
-            match, "MktGoalKeeper", controller=controller.TwoSidesLQR)
+    def __init__(self, match, plot_field=True):
+        super().__init__(match, "MktGoalKeeper", controller=controller.TwoSidesLQR)
 
     def start(self, robot=None):
         super().start(robot=robot)
 
+        print(robot.robot_id)
+
         self.restrict = algorithms.fields.PotentialField(self.match, name="RestrictBehaviour")
+
+        self.project = algorithms.fields.PotentialField(self.match, name="ProjectBehaviour")
 
         x, y, w, h = self.match.game.field.get_small_area("defensive")
 
@@ -22,33 +30,82 @@ class mktGoalKeeper(Strategy):
                 self.match,
                 target = (w, y+h/2),
                 theta = math.pi/2,
-                line_size = 1.3,
+                line_size = 1.3/2,
                 line_dist = 0.15,
                 line_dist_max = 0.3,
-                inverse = True,
                 line_dist_single_side = True,
-                multiplier = 0.5,
+                inverse = True,
+                multiplier = 0.75,
                 decay = lambda x : x
             )
         )
 
         self.restrict.add_field(
-            algorithms.fields.PointField(
+            algorithms.fields.LineField(
                 self.match,
-                target = (0, 0),
-                radius = .12,
-                radius_max = .12,
-                decay = lambda x : 1,
-                multiplier = -.75
+                target = (0, y+h/2),
+                theta = 3*math.pi/2,
+                line_size = h/2,
+                line_dist = 0.075,
+                line_dist_max = 0.1,
+                line_dist_single_side = True,
+                inverse = True,
+                multiplier = 0.75,
+                decay = lambda x : x
             )
         )
 
+        self.restrict.add_field(
+            algorithms.fields.LineField(
+                self.match,
+                target = (0, 0.25),
+                theta = math.pi,
+                line_size = 1.3,
+                line_dist = 0.1,
+                line_dist_max = 0.25,
+                line_dist_single_side = True,
+                inverse = True,
+                multiplier = 0.75,
+                decay = lambda x : x
+            )
+        )
+
+        self.restrict.add_field(
+            algorithms.fields.LineField(
+                self.match,
+                target = (0, 1.3-0.25),
+                theta = 0,
+                line_size = 1.3,
+                line_dist = 0.1,
+                line_dist_max = 0.25,
+                line_dist_single_side = True,
+                inverse = True,
+                multiplier = 0.75,
+                decay = lambda x : x
+            )
+        )
+
+        self.project.add_field(self.restrict)
+
+        self.project.add_field(
+            algorithms.fields.PointField(
+                self.match,
+                target = lambda m : (w/2, m.ball.y),
+                radius = 0.05,
+                decay = lambda x : 1,
+                multiplier = 0.5
+            )
+        )
 
     def decide(self):
 
         behaviour = None
 
-        behaviour = self.restrict
+        if self.match.ball.x <= 0.750:
+            behaviour = self.project
+        else:
+            behaviour = self.restrict
+
+        #return super().decide(self.project)
 
         return behaviour.compute([self.robot.x, self.robot.y])
-
