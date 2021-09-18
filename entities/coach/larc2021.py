c35c20d48c96e944b94c8e91e97ec575bdada50f
Alexsandro Santos <alexsandro2134@gmail.com>
2021-09-18 13:40:44 -0300

    add missing files 2

---
+++ entities/coach/larc2021.py
@@ -0,0 +1,25 @@
+from entities.coach.coach import BaseCoach
+from entities import plays
+
+class Coach(BaseCoach):
+    NAME = "LARC_2021"
+    def __init__(self, match):
+        super().__init__(match)
+
+        self.playbook = plays.Playbook(self)
+
+        main_play = plays.larc2021.MainPlay(self)
+        
+        self.playbook.add_play(main_play)
+        self.playbook.set_play(main_play)
+    
+    def get_positions(self, foul, team_color, foul_color):
+        team = self.positions.get(team_color)
+        foul = team.get(foul)
+        replacements = foul.get(foul_color, foul.get("POSITIONS"))
+        return replacements
+
+    def decide (self):
+        self.playbook.update()
+
+    
