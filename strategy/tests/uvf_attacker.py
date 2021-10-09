5f6f689b2e859d70e93cd3aa5435252270f9a795
Ana-Monici <ana.monici@outlook.com>
2021-10-09 16:21:28 -0300

    Add astar ufv attacker.

--- strategy/tests/uvf_attacker.py
+++ strategy/tests/uvf_attacker.py
@@ -1,20 +1,122 @@
+from controller.simple_LQR import SimpleLQR
 import math
 import algorithms
 from commons import math as nfc_math
 from strategy.BaseStrategy import Strategy
 from controller import TwoSidesLQR
 from strategy.DebugTools import DebugPotentialFieldStrategy
+from algorithms.astar import AStar, Node
+from algorithms.astar.fieldGraph import FieldGraph
+from scipy.spatial import Voronoi
 
 
-# class Attacker(DebugPotentialFieldStrategy):
+#class Attacker(DebugPotentialFieldStrategy):
 class Attacker(Strategy):
-    def __init__(self, match):
+    def __init__(self, match, name = 'UFV-Attacker'):
         self.ctrl_params = {"l": 0.07}
         super().__init__(match,
             name=name,
             controller=TwoSidesLQR,
             controller_kwargs=self.ctrl_params
         )
+
+    def voronoi_astar(self, m):
+        self.graph = FieldGraph()
+
+        self.robot_node = Node([self.robot.x, self.robot.y])
+        self.graph.set_start(self.robot_node)
+
+        self.robot_node.position = [self.robot.x, self.robot.y]
+
+        corners = [
+            [self.match.game.field.get_dimensions()[0], 0],
+            [0, 0],
+            [0, self.match.game.field.get_dimensions()[1]],
+            [self.match.game.field.get_dimensions()[0], self.match.game.field.get_dimensions()[1]],
+
+            [0, self.match.game.field.get_dimensions()[1]/2],
+            [self.match.game.field.get_dimensions()[0], self.match.game.field.get_dimensions()[1]/2],
+
+            [0, self.match.game.field.get_dimensions()[1]/4],
+            [self.match.game.field.get_dimensions()[0], self.match.game.field.get_dimensions()[1]/4],
+
+            [0, 3 * self.match.game.field.get_dimensions()[1]/4],
+            [self.match.game.field.get_dimensions()[0], 3 * self.match.game.field.get_dimensions()[1]/4],
+        ]
+        mergeble_obstacles = [ [r.x, r.y] for r in self.match.opposites] + [
+            [r.x, r.y] for r in self.match.robots 
+            if r.robot_id != self.robot.robot_id
+        ]
+
+        dist = lambda x, y: math.sqrt( (x[0] - y[0])**2 + (x[1] - y[1])**2 )
+        to_merge = []
+        to_remove = []
+        for i1, m1 in enumerate(mergeble_obstacles):
+            for i2, m2 in enumerate(mergeble_obstacles):
+                if m1 == m2: continue
+                if dist(m1, m2) <= 2 * self.robot.dimensions["L"] * math.sqrt(2):
+                    to_merge.append([m1, m2])
+                    to_remove.append(i1)
+                    to_remove.append(i2)
+        
+        to_remove = list(set(to_remove))
+        for index in sorted(to_remove, reverse=True):
+            del mergeble_obstacles[index]
+
+        for o1, o2 in to_merge:
+            mergeble_obstacles.append([ (o1[0] + o2[0])/2 , (o1[1] + o2[1])/2 ])
+
+        unmergeble_obsctacles = [ [self.match.ball.x, self.match.ball.y], self.robot_node.position]
+
+
+        obstacles = corners + mergeble_obstacles + unmergeble_obsctacles
+
+        vor = Voronoi(obstacles)
+
+        objective = self.match.ball
+        target_node = Node([objective.x, objective.y])
+
+        nodes = [
+            Node([a[0], a[1]]) for a in vor.vertices
+        ] + [
+            target_node, self.robot_node
+        ]
+
+        objective_index = len(obstacles) - 2
+        robot_index = len(obstacles) - 1
+        
+        self.graph.set_nodes(nodes)
+
+        polygon_objective_edges = []
+        polygon_robot_edges = []
+
+        for edge, ridge_vertice in zip(vor.ridge_vertices, vor.ridge_points):
+            if edge[0] == -1: continue
+            self.graph.add_edge([nodes[edge[0]], nodes[edge[1]]])
+
+            if objective_index in ridge_vertice:
+                polygon_objective_edges.append(nodes[edge[0]])
+                polygon_objective_edges.append(nodes[edge[1]])
+
+            if robot_index in ridge_vertice:
+                polygon_robot_edges.append(nodes[edge[0]])
+                polygon_robot_edges.append(nodes[edge[1]])
+            
+            if objective_index in ridge_vertice and robot_index in ridge_vertice:
+                self.graph.add_edge([self.robot_node, target_node])
+
+        for edge_to_ball in set(polygon_objective_edges):
+            self.graph.add_edge([edge_to_ball, target_node])
+
+        for edge_to_ball in set(polygon_robot_edges):
+            self.graph.add_edge([edge_to_ball, self.robot_node])
+
+
+        path = AStar(self.robot_node, target_node).calculate()
+
+        dist = ((path[0][0] - path[1][0])**2 + (path[1][1] - path[1][1])**2 )**.5
+
+        return  path[1]
 
     def start(self, robot=None):
         super().start(robot=robot)
@@ -46,6 +148,11 @@ class Attacker(Strategy):
         self.avoid_area = algorithms.fields.PotentialField(
             self.match,
             name="{}|AvoidAreaBehaviour".format(self.__class__)
+        )
+
+        self.astar = algorithms.fields.PotentialField(
+            self.match,
+            name="{}|AstarBehaviour".format(self.__class__)
         )
 
         def shifted_target_left(m, radius_2=uvf_radius_2):
@@ -196,20 +303,44 @@ class Attacker(Strategy):
                 o_p[1]  + p_s[1]
             ]
 
-        for robot in self.match.robots + self.match.opposites:
-            if robot.get_name() == self.robot.get_name():
-                continue
+        
 
-        self.seek.add_field(
+        # for robot in self.match.robots + self.match.opposites:
+        #     if robot.get_name() == self.robot.get_name():
+        #         continue
+        #     self.seek.add_field(
+        #         algorithms.fields.PointField(
+        #             self.match,
+        #             target = lambda m, o=robot, r=self.robot, k=avoiance_K: (
+        #                 proj_obstacle(m, o, r, k)
+        #             ),
+        #             radius = .2,
+        #             radius_max = .2,
+        #             decay = lambda x: x**4,
+        #             multiplier = lambda m, r=self.robot: -2 if ((r.x - m.ball.x)**2 +  (r.y - m.ball.y)**2)**.5 > 0.15 else 0
+        #         )
+        #     )
+        #     self.seek.add_field(
+        #         algorithms.fields.TangentialField(
+        #             self.match,
+        #             target = lambda m, o=robot, r=self.robot, k=avoiance_K: (
+        #                 proj_obstacle(m, o, r, k)
+        #             ),
+        #             radius = .21,
+        #             radius_max = .16,
+        #             decay = lambda x: x,
+        #             clockwise = True,
+        #             multiplier = lambda m, r=self.robot: 1.5 if ((r.x - m.ball.x)**2 +  (r.y - m.ball.y)**2)**.5 > 0.15 else 0
+        #         )
+        #     )
+
+        self.astar.add_field(
             algorithms.fields.PointField(
                 self.match,
-                target = lambda m, o=robot, r=self.robot, k=avoiance_K: (
-                    proj_obstacle(m, o, r, k)
-                ),
-                radius = .08,
-                radius_max = .08,
-                decay = lambda x: 1,
-                multiplier = lambda m, r=self.robot: -.2 if ((r.x - m.ball.x)**2 +  (r.y - m.ball.y)**2)**.5 > 0.15 else 0
+                target = lambda m, r=self : r.voronoi_astar(m),
+                radius = .075,
+                decay = lambda x: x,
+                multiplier = 0.8
             )
         )
 
@@ -229,11 +360,17 @@ class Attacker(Strategy):
             small_area[3] + self.robot.dimensions["L"]/2
         ]
         ball = [self.match.ball.x, self.match.ball.y]
+        
+        dist_to_ball = ( (ball[0] - self.robot.x)**2 +  (ball[1] - self.robot.y)**2 )**.5
+        tangential_radius = 0.3
 
         if nfc_math.point_in_rect(ball, goal_area):
             behaviour = self.wait
+        elif dist_to_ball >= tangential_radius:
+            behaviour = self.astar
         else:
             behaviour = self.seek
 
         return behaviour.compute([self.robot.x, self.robot.y])
+        #return super().decide(behaviour)
 
