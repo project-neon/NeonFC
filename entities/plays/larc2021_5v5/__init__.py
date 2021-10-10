a780bbce357aab5772de1e2081e3e22f65b9b496
Alexsandro Santos <alexsandro2134@gmail.com>
2021-10-10 17:37:53 -0300

    Add 5v5 plays

---
+++ entities/plays/larc2021_5v5/__init__.py
@@ -0,0 +1,3 @@
+from entities.plays.larc2021_5v5.mainPlay import MainPlay
+from entities.plays.larc2021_5v5.penaltyPlay import PenaltyPlay
+from entities.plays.larc2021_5v5.goalKickPlay import GoalKickPlay
