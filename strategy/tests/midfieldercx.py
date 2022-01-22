dec6b46218bde647712fdbdf192764b7f64bc6ff
gTorrez <g.ctorres09@gmail.com>
2022-01-22 17:08:16 -0300

    changes in midfielder position

--- strategy/tests/midfieldercx.py
+++ strategy/tests/midfieldercx.py
@@ -7,16 +7,14 @@ from strategy.DebugTools import DebugPotentialFieldStrategy
 from commons.math import point_in_rect
 
 
-class MidFielder2(Strategy):
-    def __init__(self, match, name = 'MidFielder2'):
+class MidFielderkkkk(Strategy):
+    def __init__(self, match, name = 'MidFielderkkkk'):
         self.ctrl_params = {"l": 0.07}
         super().__init__(match,
             name=name,
             controller=controller.TwoSidesLQR,
             controller_kwargs=self.ctrl_params
         )
-
-
     
     def start(self, robot=None):
         super().start(robot=robot)
@@ -34,6 +32,8 @@ class MidFielder2(Strategy):
         self.push = algorithms.fields.PotentialField(self.match, name="PushBehaviour")
 
         self.avoid_area = algorithms.fields.PotentialField(self.match, name="AvoidAreaBehaviour")
+
+        self.attack = algorithms.fields.PotentialField(self.match, name="AttackBehaviour")
         
         #small area x, y, width and height
         self.sa_x, self.sa_y, self.sa_w, self.sa_h = self.match.game.field.get_small_area("defensive")
@@ -62,14 +62,13 @@ class MidFielder2(Strategy):
             else:
                 return False
 
-        # novo (2k22)
         def sobra(m):    
 
             for r in self.match.robots:
-                if r.strategy.name in ["UVF-Attacker", "shooter", "AtacanteMeu"]:
+                if r.strategy.name in ["UVF-Attacker", "shooter"]:
                     self.atk_x, self.atk_y = r.x, r.y
             
-            if m.ball.x >= self.field_w/2:  
+            if m.ball.x >= self.field_w/2 - 0.2:  
                 # top corner
                 if point_in_rect((m.ball.x, m.ball.y), (self.field_w-self.sa_w, self.field_h/2, self.sa_w, self.field_h/2)):
                     x = self.field_w - self.sa_w - 0.2
@@ -81,39 +80,45 @@ class MidFielder2(Strategy):
                     y = self.field_h/2 - 0.1
 
                 else:
+                    if m.ball.x < self.atk_x:
+                        ref_x = m.ball.x
+                        ref_y = m.ball.y
+                    else:
+                        ref_x = self.atk_x
+                        ref_y = self.atk_y
                     # terço maior do campo
                     if m.ball.y > self.field_h * 2/3:
                         # se o atacante vem de baixo
                         if self.atk_y < self.field_h * 2/3:
-                            x = min(m.ball.x, self.atk_x) - 0.4
+                            x = ref_x - 0.4
                             y = self.field_h * 5/6
                         # se o atacante vem de cima
                         else:
-                            x = min(m.ball.x, self.atk_x) - 0.4
-                            y = m.ball.y - 0.3
+                            x = ref_x - 0.4
+                            y = ref_y - 0.3
 
                     # segundo terço do campo
                     elif m.ball.y > self.field_h*1/3 and m.ball.y < self.field_h*2/3:
                         # se o atacante vem de baixo ou de cima 
                         if self.atk_y < self.field_h * 1/3 or self.atk_y > self.field_h * 2/3:
-                            x = min(m.ball.x, self.atk_x) - 0.4
+                            x = ref_x - 0.4
                             y = self.field_h/2
                         # se o atacante esta no meio
                         else :
-                            x = min(m.ball.x, self.atk_x) - 0.4
-                            y = m.ball.y
+                            x = ref_x - 0.4
+                            y = ref_y
                     # primeiro terço do campo
                     else:
                         # se o atacante vem de cima
                         if self.atk_y > self.robot.y:
-                            x = min(m.ball.x, self.atk_x) - 0.4
+                            x = ref_x - 0.4
                             y = self.field_h * 1/6
                         # se o atacante vem de baixo
                         else:
-                            x = min(m.ball.x, self.atk_x) - 0.4
-                            y = m.ball.y + 0.3
+                            x = ref_x - 0.4
+                            y = ref_y + 0.3
             # ????
-            elif m.ball.x < self.field_w/2 and not is_in_defensive_area(m):
+            elif m.ball.x < self.field_w/2 - 0.2 and not is_in_defensive_area(m):
                     # defense top corner or bottom corner
                     # if is_in_defensive_corner(m):
                 if m.ball.y < self.field_h/2:
@@ -187,6 +192,16 @@ class MidFielder2(Strategy):
             )
         )
 
+        self.attack.add_field(
+            algorithms.fields.PointField(
+                self.match,
+                target = lambda m: (m.ball.x, m.ball.y),
+                radius = 0.1,
+                multiplier = VEL,
+                decay = lambda x : x
+            )
+        )
+
 
     # melhorar posicionamento do atacante e meio campo
     def decide(self):
@@ -194,18 +209,25 @@ class MidFielder2(Strategy):
         behaviour = None
         self.theta = self.robot.theta
 
+        def dist_to_ball(robot_y, robot_x):
+            dist = ((robot_y - ball.y)**2 + (robot_x - ball.x)**2)**0.5
+            return dist
+
+        atk_dist = dist_to_ball(self.atk_y, self.atk_x)
+        midf_dist = dist_to_ball(self.robot.y, self.robot.x)
+
         def is_atk_in_area():
             if self.atk_x >= self.field_w - self.sa_w and self.atk_y >= self.sa_y and self.atk_y <= self.sa_h + self.sa_y:
                 return True
             else:
                 return False
-        
-        if (ball.x >= self.field_w - self.sa_w - 0.3 and ball.y > self.field_h/2-0.25 and 
-              ball.y < self.field_h/2+0.25) and not is_atk_in_area():
-            behaviour = self.push
 
-        elif self.robot.x < self.field_w/2 and ball.x >= self.field_w/2:
-            behaviour = self.sobra_protect
+        if ball.vx < 0 and self.robot.x < ball.x and self.atk_x > ball.x and atk_dist > midf_dist:
+            behaviour = self.attack
+        
+        elif (ball.x >= self.field_w - self.sa_w - 0.3 and ball.y > self.field_h/2-0.2 and 
+              ball.y < self.field_h/2+0.2) and not is_atk_in_area():
+            behaviour = self.push
 
         else:
             behaviour = self.sobra
@@ -214,17 +236,27 @@ class MidFielder2(Strategy):
 
     def spin(self):
 
-        if point_in_rect((self.robot.x, self.robot.y), (self.sa_w - 0.0375, self.field_h - 0.1875, 0.075, 0.075)):
+        if point_in_rect((self.robot.x, self.robot.y), (self.sa_w -0.07 - 0.0375, self.field_h -0.07 - 0.1875, 0.075, 0.075)):
             w = ((self.theta**2)**0.5 - 0.785) * 20
+
+        elif point_in_rect((self.robot.x, self.robot.y), (self.sa_w - 0.07 - 0.0375, 0.1125 - 0.07, 0.075, 0.075)):
+            w = ((self.theta**2)**0.5 - 2.3558) * 20
+
         else:
-            w = ((self.theta**2)**0.5 + 0.785) * 5
+            return False
 
         return -w, w
 
     def spinning_time(self):
       
-        if point_in_rect((self.robot.x, self.robot.y), (self.sa_w - 0.0375, self.field_h - 0.1875, 0.075, 0.075)):
+        if point_in_rect((self.robot.x, self.robot.y), (self.sa_w - 0.07 -  0.0375, self.field_h - 0.07 - 0.1875, 0.075, 0.075)):
             if (self.theta < -0.7504 and self.theta > -0.8203):
+                return False
+            else:
+                return True
+
+        elif point_in_rect((self.robot.x, self.robot.y), (self.sa_w - 0.07 -0.0375, 0.1125 - 0.07, 0.075, 0.075)):
+            if (self.theta < -2.321 and self.theta > -2.3911):
                 return False
             else:
                 return True
