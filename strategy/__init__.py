1a038cae0bdf1e8746672a74ed9d60543c5c988f
Alexsandro Santos <alexsandro2134@gmail.com>
2022-05-15 15:45:01 -0300

    minor refactor on coaches and clean old strategies

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -2,7 +2,7 @@ from strategy import tests
 from strategy import larc2020
 from strategy import larc2021
 from strategy import iron2021
-from strategy import alex
+from strategy import cbfr2022
 from strategy.BaseStrategy import Strategy
 
 # debug tools
