3b2e871493ab1f41120812a9d5e20e6c3b68d7d9
Ana-Monici <ana.monici@outlook.com>
2022-06-27 20:28:41 -0300

    Added a* to the defend behaviour of the suporting robots.

--- strategy/cbfrs2022_5v5/leftWing.py
+++ strategy/cbfrs2022_5v5/leftWing.py
@@ -19,61 +19,11 @@ class LeftWing(Strategy):
     def start(self, robot=None):
         super().start(robot=robot)
 
-        self.defend = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|DefendBehaviour".format(self.__class__)
-        )
-
-        self.recover = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|DefendBehaviour".format(self.__class__)
-        )
-
-        self.combat = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|CombatBehaviour".format(self.__class__)
-        )
-
-        self.attack = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|AttackBehaviour".format(self.__class__)
-        )
-
-        def defen_pos(m):
-            if m.ball.y < self.g_lwr:
-                return (m.ball.x - 1, self.field_h/2 + 0.15)
-            else:
-                return (m.ball.x, (self.sa_y + self.sa_h + 0.2))
-
-        self.defend.add_field(
-            algorithms.fields.PointField(
-                self.match,
-                target = defen_pos,
-                radius = .075,
-                decay = lambda x: x**6,
-                multiplier = 1
-            )
-        )
-
-        # self.combat.add_field(
-        #     algorithms.fields.PointField(
-        #         self.match,
-        #         target = lambda m: (m.ball.x - 0.4, m.ball.y),
-        #         radius = 2.2,
-        #         decay = lambda x: 1,
-        #         multiplier = 1
-        #     )
-        # )
-
-        # self.attack.add_field(
-        #     algorithms.fields.PointField(
-        #         self.match,
-        #         target = lambda m: (m.ball.x - 0.6, (self.g_hgr + 0.1)),
-        #         radius = .075,
-        #         decay = lambda x: x**6,
-        #         multiplier = 1
-        #     )
-        # )
+    def defend_position(self, match):
+        if match.ball.y < self.g_lwr:
+            return [match.ball.x - 1, self.field_h/2 + 0.15]
+        else:
+            return [match.ball.x, (self.sa_y + self.sa_h + 0.2)]
 
     def reset(self, robot=None):
         super().reset()
@@ -101,18 +51,17 @@ class LeftWing(Strategy):
 
         if ball.y > self.g_hgr and ball.x > self.robot.x and ball.x > self.field_w/4:
             # behaviour = self.combat
-            # print(behaviour.name)
             target_x = max(ball.x - 0.4, self.sa_w + 0.10)
             return self.use_astar([ball.x - 0.4, ball.y])
         else:
             if ball.x > self.sa_w + 0.4 and ball.y < self.sa_y + self.sa_h + 0.15:
                 # behaviour = self.attack
-                # print(behaviour.name)
                 target_x = max(ball.x - 0.6, self.sa_w + 0.10)
                 return self.use_astar([target_x, (self.g_hgr + 0.1)])
             else:
-                behaviour = self.defend
+                # behaviour = self.defend
+                return self.use_astar(self.defend_position(self.match))
 
         # print(behaviour.name)
 
-        return behaviour.compute([self.robot.x, self.robot.y])
+        # return behaviour.compute([self.robot.x, self.robot.y])
--- strategy/cbfrs2022_5v5/rightAttacker.py
+++ strategy/cbfrs2022_5v5/rightAttacker.py
@@ -23,62 +23,9 @@ class RightAttacker(Strategy):
     def start(self, robot=None):
         super().start(robot=robot)
 
-        self.defend = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|PointBehaviour".format(self.__class__)
-        )
-
-        self.follow = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|FollowBehaviour".format(self.__class__)
-        )
-
-        self.attack = algorithms.fields.PotentialField(
-            self.match,
-            name="{}|AttackBehaviour".format(self.__class__)
-        )
-
         self.push = algorithms.fields.PotentialField(
             self.match,
             name="{}|PushBehaviour".format(self.__class__)
-        )
-
-        def defen_pos(m):
-            if m.ball.y > self.g_hgr:
-                return (self.sa_x + 0.25, self.field_h/2 + 0.25)
-            if m.ball.y < self.g_lwr:
-                return (self.sa_x + 0.25, self.field_h/2 - 0.25)
-            
-            return (self.sa_x + 0.4, self.field_h/2)
-
-        self.defend.add_field(
-            algorithms.fields.PointField(
-                self.match,
-                target = defen_pos,
-                radius = .075,
-                decay = lambda x: x**6,
-                multiplier = 1
-            )
-        )
-
-        self.follow.add_field(
-            algorithms.fields.PointField(
-                self.match,
-                target = lambda m: (m.ball.x - 0.3, m.ball.y),
-                radius = .075,
-                decay = lambda x: x,
-                multiplier = 1
-            )
-        )
-
-        self.attack.add_field(
-            algorithms.fields.PointField(
-                self.match,
-                target = lambda m: (m.ball.x - 0.3, (self.field_h/2 - 0.15)),
-                radius = .075,
-                decay = lambda x: x**6,
-                multiplier = 1
-            )
         )
 
         self.push.add_field(
@@ -90,19 +37,14 @@ class RightAttacker(Strategy):
                 decay = lambda x : x
             )
         )
-
-        # self.push.add_field(
-        #     algorithms.fields.LineField(
-        #         self.match,
-        #         target = lambda m: (m.ball.x, m.ball.y),
-        #         theta = lambda m: -((m.ball.x - self.robot.x)/(m.ball.y - self.robot.y)),
-        #         line_size = self.robot_w,
-        #         line_dist = self.field_w/2,
-        #         line_dist_max = self.field_w/2,
-        #         decay = lambda x: 1,
-        #         multiplier = 2,
-        #     )
-        # )
+    
+    def defend_position(self, match):
+            if match.ball.y > self.g_hgr:
+                return [self.sa_x + 0.25, self.field_h/2 + 0.25]
+            if match.ball.y < self.g_lwr:
+                return [self.sa_x + 0.25, self.field_h/2 - 0.25]
+            
+            return [self.sa_x + 0.4, self.field_h/2]
 
     def use_astar(self, target):
         target = target # target better not be the ball
@@ -138,7 +80,6 @@ class RightAttacker(Strategy):
                 behaviour = self.push
             else:
                 # behaviour = self.follow
-                # print(behaviour.name)
                 return self.use_astar([ball.x - 0.3, ball.y])
 
         # elif ball.y <= self.sa_y + self.sa_h + 0.1 and ball.y >= self.sa_y - 0.1 and ball.x > self.sa_w + 0.3:
@@ -146,12 +87,12 @@ class RightAttacker(Strategy):
         else:
             if ball.x > self.field_w/4:
                 # behaviour = self.attack
-                # print(behaviour.name)
                 if self.field_h/2-0.4 < ball.y < self.field_h/2+0.4:
                     return self.use_astar([ball.x - 0.3, ball.y])
                 return self.use_astar([ball.x - 0.3, (self.field_h/2)])
             else:
-                behaviour = self.defend
+                # behaviour = self.defend
+                return self.use_astar(self.defend_position(self.match))
 
         # print(ball.x > self.field_w-0.35 and self.field_h/2-0.4 < ball.y < self.field_h/2+0.4)
         # print(left_proj < self.g_hgr and right_proj > self.g_lwr)
