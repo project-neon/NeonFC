0f00db991d28b252b24fc732126ecf941f1ee21a
mcs010 <math.cds10@gmail.com>
2022-09-03 17:41:48 -0300

    refactor(controller): Add description to angle adjustment function in PID control

--- controller/PID_control.py
+++ controller/PID_control.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 
 def angle_adjustment(angle):
+        """Adjust angle of the robot when objective is "behind" the robot"""
         phi = angle % math.radians(360)
         if phi > math.radians(180):
             phi = phi - math.radians(360)
