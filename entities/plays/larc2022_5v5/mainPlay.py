1ad43ea5230053e74418fae485ee15de1f8c0259
mcs010 <math.cds10@gmail.com>
2022-09-21 15:23:03 -0300

    feat(plays): Add new base of a playbook with its init method for larc 2022 5v5 simulated

---
+++ entities/plays/larc2022_5v5/mainPlay.py
@@ -0,0 +1,16 @@
+import math
+import strategy
+from entities.plays.playbook import Play
+
+class MainPlay(Play):
+    def __init__(self, coach):
+        super().__init__(coach)
+        self.match = self.coach.match
+        self.coach = coach
+        self.strategies = [
+            strategy.cbfr2022.MainGoalkeeper(self.match),
+            strategy.tests.Idle(self.match),
+            strategy.tests.Idle(self.match),
+            strategy.tests.Idle(self.match),
+            strategy.tests.Idle(self.match)
+        ]
