18ebd9a357b7e592af5a8ff00a005e8a20c9c2db
mcs010 <math.cds10@gmail.com>
2022-09-21 17:03:25 -0300

    feat(coach larc2022_5v5): Add get_positions method based on cbfrs2022

--- entities/coach/larc2022_5v5.py
+++ entities/coach/larc2022_5v5.py
@@ -38,3 +38,25 @@ class Coach(BaseCoach):
         goalkick_play.add_transition(goalkick_seconds_trigger, main_play)
 
         self.playbook.set_play(main_play)
+
+    def _get_positions(self, foul, team_color, foul_color, quadrant):
+        quad = quadrant
+        foul_type = foul
+        team = self.positions.get(team_color)
+        foul = team.get(foul)
+        if not foul:
+            return None
+
+        if foul_type != "FREE_BALL":
+            replacements = foul.get(foul_color, foul.get("POSITIONS"))
+        else:
+            replacements = foul.get(f"{quad}")
+        
+        return replacements
+
+    def get_positions(self, foul, team_color, foul_color, quadrant):
+        play_positioning = self.playbook.get_actual_play().get_positions(foul, team_color, foul_color, quadrant)
+        if play_positioning:
+            return play_positioning
+        
+        return self._get_positions(foul, team_color, foul_color, quadrant)
