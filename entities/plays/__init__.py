804f55cb820daca85fdd74dd8dfb2a98b281f97b
Alexsandro Santos <alexsandro2134@gmail.com>
2022-10-17 14:09:37 -0300

    refactoring 5v5

--- entities/plays/__init__.py
+++ entities/plays/__init__.py
@@ -15,6 +15,7 @@ from entities.plays.playbook import OnPenaltyKick
 
 from entities.plays import larc2021
 from entities.plays import larc2021_5v5
+from entities.plays import larc2022_5v5
 from entities.plays import rcx2022
 from entities.plays import cbfrs2022
 from entities.plays import cbfrs2022_5v5
---
+++ entities/plays/larc2022_5v5/mainPlay.py
@@ -0,0 +1,57 @@
+import math
+import strategy
+from entities.plays.playbook import Play
+
+class MainPlay(Play):
+    def __init__(self, coach):
+        super().__init__(coach)
+        self.match = self.coach.match
+        self.coach = coach
+        self.strategies = [
+            strategy.larc2022_5v5.GoalKeeper(self.match, 'Goalkeeper'),
+            strategy.larc2022_5v5.RadialDefender(self.match, 'Defender1'),
+            strategy.larc2022_5v5.RadialDefender(self.match, 'Defender2'),
+            strategy.larc2022_5v5.SecondAttacker(self.match, 'SecondAttacker'),
+            strategy.larc2022_5v5.MainAttacker(self.match, 'MainAttacker'),
+        ]
+
+    def _can_play(self):
+        return self.match.game.referee.can_play()
+
+    def start_up(self):
+        super().start_up()
+
+    def update(self):
+        super().update()
+        
+        attackers = self.match.robots[-2:]
+        best_attacker = None
+        best_fit = -9999
+        for attacker_candidate in attackers:
+            fit = self._elect_leftattacker(attacker_candidate)
+            if fit > best_fit:
+                best_attacker = attacker_candidate.robot_id
+                best_fit = fit
+        
+        if best_attacker == 3:
+            strategies = self.strategies[0:3] + [self.strategies[4], self.strategies[3]]
+        else:
+            strategies = self.strategies
+
+
+        for robot, strategy in zip(self.match.robots, strategies):
+            if robot.strategy is None:
+                robot.strategy = strategy
+                robot.start()
+            elif robot.strategy.name != strategy.name:
+                robot.strategy = strategy
+                robot.start()
+
+
+            
+    def _elect_leftattacker(self, robot):
+        is_behind = 2 if robot.x > self.match.ball.x else 1
+        dist_to_ball = math.sqrt(
+            (robot.x - self.match.ball.x)**2 + (robot.y - self.match.ball.y)**2
+        )
+        return 1000 - dist_to_ball * is_behind
