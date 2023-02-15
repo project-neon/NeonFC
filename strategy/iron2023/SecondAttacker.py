c8ed6af3639fc0eb5f0b517c59aa0c3eaa667a8c
Joao Pedro Leal <jpkleal@gmail.com>
2023-02-15 18:18:54 -0300

    feat(strategy/iron2023): create second attacker

---
+++ strategy/iron2023/SecondAttacker.py
@@ -0,0 +1,38 @@
+from algorithms.univector_field import UnivectorField
+import math
+from controller.uni_controller import UniController
+from strategy.BaseStrategy import Strategy
+from commons.math import distance_between_points
+
+class ShadowAttacker(Strategy):
+    def __init__(self, match, plot_field=False):
+        super().__init__(match, "Shadow_Attacker", controller=UniController, controller_kwargs={'control_speed': True})
+
+    def start(self, robot=None):
+        super().start(robot=robot)
+        self.univector_field = UnivectorField(n=2, rect_size=0)
+        self.dl = 0.000001
+
+    def reset(self, robot=None):
+        super().reset()
+        if robot:
+            self.start(robot)
+
+    def decide(self):
+        ball = (self.match.ball.x, self.match.ball.y)
+        main_st = [[i.x, i.y] for i in self.match.robots if i.strategy.name == "Main_Attacker"][0]
+        obs_radius = distance_between_points(main_st, ball)
+        target = main_st[:]
+        target[0] -= min(4*0.075, obs_radius)
+
+        self.univector_field.set_target(target, ball)
+        self.univector_field.del_obstacle(all=True)
+        self.univector_field.add_obstacle(main_st, 0.075*1.4, obs_radius-0.075*1.4)
+
+        x, y = self.robot.x, self.robot.y
+        theta_d = self.univector_field.compute((x, y))
+        theta_f = self.univector_field.compute((x + self.dl * math.cos(self.robot.theta), y + self.dl * math.sin(self.robot.theta)))
+
+        self.controller.target = target
+
+        return theta_d, theta_f
