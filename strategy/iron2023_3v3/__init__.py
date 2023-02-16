90e0efabb3591e8bd737fce4b9c1434f2ebed5d2
mcs010 <math.cds10@gmail.com>
2023-02-16 17:28:34 -0300

    fix(iron2023_strategy): Correct line of main attacker strategy due to game not running

--- strategy/iron2023_3v3/__init__.py
+++ strategy/iron2023_3v3/__init__.py
@@ -1,6 +1,6 @@
 from strategy.larc2022_5v5.mainGoalkeeper import GoalKeeper
 
-#from strategy.larc2022_5v5.mainAttacker import MainAttacker 
+from strategy.iron2023_3v3.mainAttacker import MainAttacker 
 from strategy.larc2022_5v5.secondAttacker import SecondAttacker 
 
 from strategy.larc2022_5v5.radialDefender import RadialDefender 
