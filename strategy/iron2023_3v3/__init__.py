ab429245bd9c37a468ec77220877d4c58ce2b14b
mcs010 <math.cds10@gmail.com>
2023-02-16 17:45:14 -0300

    feat(iron2023): Add base goalkeeper strategy

--- strategy/iron2023_3v3/__init__.py
+++ strategy/iron2023_3v3/__init__.py
@@ -1,4 +1,4 @@
-from strategy.larc2022_5v5.mainGoalkeeper import GoalKeeper
+from strategy.iron2023_3v3.mainGoalkeeper import GoalKeeper
 
 from strategy.iron2023_3v3.mainAttacker import MainAttacker 
 from strategy.larc2022_5v5.secondAttacker import SecondAttacker 
