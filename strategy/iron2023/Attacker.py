bee519893e28f9dcb01682ea1b89d922a3c0b125
Joao Pedro Leal <jpkleal@gmail.com>
2023-02-17 18:52:46 -0300

    feat(strategy/iron2023): implement shooting for main attacker

--- strategy/iron2023/Attacker.py
+++ strategy/iron2023/Attacker.py
@@ -2,6 +2,8 @@ from algorithms.limit_cycle import LimitCycle
 import math
 from controller.PID_control import PID_W_control
 from strategy.BaseStrategy import Strategy
+from commons.math import distance_between_points
+
 
 class Attacker_LC(Strategy):
     def __init__(self, match, plot_field=False):
@@ -10,7 +12,6 @@ class Attacker_LC(Strategy):
     def start(self, robot=None):
         super().start(robot=robot)
         self.limit_cycle = LimitCycle(self.match)
-
         self.field_w, self.field_h = self.match.game.field.get_dimensions()
 
     def reset(self, robot=None):
@@ -20,10 +21,14 @@ class Attacker_LC(Strategy):
 
     def decide(self):
         ball = (self.match.ball.x, self.match.ball.y)
+        robot = (self.robot.x, self.robot.y)
 
-        self.limit_cycle.set_target(ball)
-        self.limit_cycle.add_obstacle(self.get_virtual_obstacle(ball))
-        desired = self.limit_cycle.compute(self.robot)
+        if distance_between_points(ball, robot) < 0.1 and not self.behind_ball():
+            desired = (1.5, 0.65)
+        else:
+            self.limit_cycle.set_target(ball)
+            self.limit_cycle.add_obstacle(self.get_virtual_obstacle(ball))
+            desired = self.limit_cycle.compute(self.robot, fitness=20)
 
         return desired
 
