bbfbd9936427e15f64306bfecc5ecfcce609c818
Joao Pedro Leal <jpkleal@gmail.com>
2023-02-25 09:16:22 -0300

    fix(strategy/iron2023): fix shadowstriker defensive position logic

--- strategy/iron2023/SecondAttacker.py
+++ strategy/iron2023/SecondAttacker.py
@@ -73,7 +73,7 @@ class Wait(PlayerPlay):
         b = (.35, 0.2)
 
         c = (self.robot.x, self.robot.y)
-        d = [(r.x, r.y) for r in self.match.robots if r.strategy.name == "Shadow_Attacker"][0]
+        d = [(r.x, r.y) for r in self.match.robots if r.strategy.name == "Main_Attacker"][0]
 
         # Calculate the distances between each robot and each fixed point
         distance_c_a = math.sqrt((c[0] - a[0]) ** 2 + (c[1] - a[1]) ** 2)
