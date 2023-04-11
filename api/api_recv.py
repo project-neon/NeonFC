1e737c6c1fe60878232bc8c54709966d8e1b00a4
Joao Pedro Leal <jpkleal@gmail.com>
2023-04-11 20:06:18 +0000

    feat(api): make api_recv more versatile

--- api/api_recv.py
+++ api/api_recv.py
@@ -12,6 +12,7 @@ class Api_recv(threading.Thread):
         self.address = address
         self.port = port
         self.buffer_size = BUFFER_SIZE
+        self.decod_data = None
         
 
     # Receives data
@@ -27,18 +28,20 @@ class Api_recv(threading.Thread):
             # Feedback commands from socket (e.g. an interface)
             #print(decoded_data)
 
-            team_color = decoded_data.get('TEAM_COLOR')
-            team_side = decoded_data.get('TEAM_SIDE')
-            game_status = decoded_data.get('GAME_STATUS')
+            team_color = decoded_data.get('TEAM_COLOR', None)
+            team_side = decoded_data.get('TEAM_SIDE', None)
+            game_status = decoded_data.get('GAME_STATUS', None)
 
             # Change team color - Doesnt need to return color because it already changes on match team color inside the function
-            if team_color != self.match.team_color:
+            if team_color and team_color != self.match.team_color:
                 self.match.restart(team_color)
             
             # Stop game and game on
-            if game_status != self.match.game_status:
+            if game_status and game_status != self.match.game_status:
                 self.match.game_status = game_status
 
             # Change team side - returns team side
-            if team_side != self.match.team_side:
+            if team_side and team_side != self.match.team_side:
                 self.match.team_side = team_side
+
+            self.decod_data = decoded_data
