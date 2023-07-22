f6941d8e97e3c7f978586036224da586118d1b68
Joao Pedro Leal <jpkleal@gmail.com>
2023-07-22 11:43:47 -0300

    feat(twiddle): update pid tunner to make it more versatile

--- strategy/tests/PID_tuner.py
+++ strategy/tests/PID_tuner.py
@@ -9,14 +9,16 @@ import math
 class PIDTuner(Strategy):
     def __init__(self, match):
         super().__init__(match, "PID_Tune", controller=PID_W_control,
-                         controller_kwargs={'V_MAX': 0, 'V_MIN': 0, 'KP': 0, 'KI': 0, 'KD': 0})
+                         controller_kwargs={'V_MAX': 0, 'V_MIN': 50, 'KP': -3, 'KI': 0, 'KD': 0})
+
         self.sender = Api("127.0.0.1", 43210)
         self.sender.start()
         self.reciver = Api_recv(match, "127.0.0.1", 43221)
         self.reciver.start()
-        self.circuit = [(.2, .65), (1.1, .65)]  # , (.4, .90), (.4, .40)]
-        self.circuit = deque(self.circuit)
+        self.circuit = []
         self.t0 = time.time()
+        self.state = "countdown"
+        self.still = True
         print("on init")
 
     def start(self, robot=None):
@@ -39,12 +41,48 @@ class PIDTuner(Strategy):
 
         return self.circuit[0]
 
+    def make_hexagonal_path(self):
+        if .15 <= self.robot.x <= .3 and .575 <= self.robot.y <= .725:
+
+            # set points on the hexagonal path that the robot will perform
+            A = (self.robot.x, self.robot.y)
+            B = (self.robot.x + .425, self.robot.y + .425)
+            C = (self.robot.x + .625, self.robot.y + .425)
+            D = (self.robot.x + 1.05, self.robot.y)
+            E = (self.robot.x + .625, self.robot.y - .425)
+            F = (self.robot.x + .425, self.robot.y - .425)
+
+            # set the circuit
+            self.circuit = [B, C, D, E, F, A]
+
+    def make_line_path(self):
+        size = .5
+
+        start = self.robot.x, self.robot.y
+        mid = self.robot.x + size*math.cos(self.robot.theta), self.robot.y + size*math.sin(self.robot.theta)
+
+        if .15 < mid[0] < 1.35 and .15 < mid[1] < 1.15:
+            self.circuit = [mid, start]
+        else:
+            mid = self.robot.x + size * math.cos(self.robot.theta - math.pi), self.robot.y + size * math.sin(self.robot.theta - math.pi)
+            if .15 < mid[0] < 1.35 and .15 < mid[1] < 1.15:
+                self.circuit = [mid, start]
+
+    def make_inplace(self):
+        self.controller.V_MAX = 0
+        self.controller.V_MIN = 0
+        self.still = True
+
+        self.circuit = [(self.robot.x + 1, self.robot.y), (self.robot.x, self.robot.y + 1)]
+
     def decide(self):
+        target = self.robot
         data = {'pid': {
                     'set_point': 0,
-                    'error': self.controller.error,
+                    'error': abs(self.controller.error),
                     'time': time.time(),
-                    'w': self.robot.vtheta},
+                    'w': self.robot.vtheta,
+                    'running': self.state == "running"},
                 'pos':{
                     'x': self.robot.x,
                     'y': self.robot.y,
@@ -55,45 +93,67 @@ class PIDTuner(Strategy):
         self.sender.send_custom_data(data)
 
         data = self.reciver.decod_data
-        new_ks = False
 
         if data:
             if self.controller.KP != data['kp']:
                 print(self.controller.KP, data['kp'])
                 self.controller.KP = data['kp']
-                new_ks = True
+                self.state = "position"
             if self.controller.KI != data['ki']:
                 print(self.controller.KI, data['ki'])
                 self.controller.KI = data['ki']
-                new_ks = True
+                self.state = "position"
             if self.controller.KD != data['kd']:
                 print(self.controller.KI, data['ki'])
                 self.controller.KD = data['kd']
-                new_ks = True
+                self.state = "position"
 
-        if new_ks:
-            print("new ks")
+        if self.state == "position":
             self.controller.alpha_old = 0
             self.controller.int_alpha = 0
-            new_ks = False
 
-            self.t0 = time.time()
+            self.make_inplace()
+            print("position")
+            if len(self.circuit) > 0:
+                print(self.circuit)
+                self.state = "countdown"
+                self.t0 = time.time()
 
-        if time.time() - self.t0 <= 1:
-            target = [.5 * self.robot.x, .5 * self.robot.y]
+        if self.state == "countdown":
+            if time.time() - self.t0 <= 0: # 3s
+                print("countdown")
+                target = self.robot
+            else:
+                self.make_inplace()
+                self.state = "running"
+                self.t0 = time.time()
 
-        elif time.time() - self.t0 <= 2:
-            target = [self.robot.x + math.cos(self.robot.theta + .5 * math.pi),
-                      self.robot.y + math.sin(self.robot.theta + .5 * math.pi)]
-            self.lp = [self.robot.x, self.robot.y, self.robot.theta]
+        if self.state == "running":
+            print(self.circuit)
+            if not self.circuit:
+                self.state = "wait"
 
-        elif time.time() - self.t0 <= 3:
-            target = [self.lp[0] + math.cos(self.lp[2] + .5 * math.pi),
-                      self.lp[1] + math.sin(self.lp[2] + .5 * math.pi)]
+            elif self.still:
+                target = self.circuit[0]
 
-        else:
-            # self.controller.KP, self.controller.KI, self.controller.KD = 0, 0, 0
+                if time.time() - self.t0 > 1:
+                    print("reached")
+                    self.t0 = time.time()
+                    self.circuit.pop(0)
+            else:
+                target = self.circuit[0]
+
+                dx = self.circuit[0][0] - self.robot.x
+                dy = self.circuit[0][1] - self.robot.y
+
+                if math.sqrt(dx ** 2 + dy ** 2) < 0.05:
+                    print("reached")
+                    self.circuit.pop(0)
+
+                self.controller.V_MAX = 50
+
+        if self.state == "wait":
             target = self.robot
-
+            self.controller.V_MAX = 0
 
         return target
