8d82d7bee155c09a5d09a63c99e583c95ca3aa8f
Joao Pedro Leal <jpkleal@gmail.com>
2023-07-22 18:19:59 -0300

    feat(rsm_2023): make attacker univector with unicontroller

--- strategy/rcx2023/Attacker.py
+++ strategy/rcx2023/Attacker.py
@@ -1,7 +1,9 @@
 import numpy as np
 from algorithms.limit_cycle import LimitCycle
+from algorithms.univector_field import UnivectorField
 import math
 from controller.PID_control import PID_W_control, PID_control
+from controller.uni_controller import UniController
 from strategy.BaseStrategy import Strategy
 from strategy.utils.player_playbook import PlayerPlay, PlayerPlaybook, OnInsideBox, OnNextTo, AndTransition
 from commons.math import distance_between_points
@@ -11,105 +13,42 @@ class MainPlay(PlayerPlay):
     def __init__(self, match, robot):
         # super().__init__(match, "Main_Attacker", controller=PID_W_control)
         super().__init__(match, robot)
+        self.dl = 0.000001
 
     def get_name(self):
         return f"<{self.robot.get_name()} Main Attacker Planning>"
 
     def start_up(self):
         super().start_up()
-        controller = PID_W_control
+        controller = UniController
         self.robot.strategy.controller = controller(self.robot)
 
     def start(self):
-        self.limit_cycle = LimitCycle(self.match)
+        self.univector = UnivectorField(n=6, rect_size=.1)
         self.field_w, self.field_h = self.match.game.field.get_dimensions()
 
     def update(self):
-        ball = (self.match.ball.x, self.match.ball.y)
-        robot = (self.robot.x, self.robot.y)
+        ball_x, ball_y = self.match.ball.x, self.match.ball.y
+        theta_ball = math.atan2(0.65 - ball_y, 1.6 - ball_x)
+        ball_rx, ball_ry = ball_x + .05 * math.cos(theta_ball), ball_y + .05 * math.sin(theta_ball)
 
-        if distance_between_points(ball, robot) < 0.1 and not self.behind_ball():
-            desired = (1.5, 0.65)
-        else:
-            self.limit_cycle.set_target(ball)
-            self.limit_cycle.add_obstacle(self.get_virtual_obstacle(ball))
-            try:
-                desired = self.limit_cycle.compute(self.robot, fitness=20)
-            except ZeroDivisionError:
-                print("ZeroDivisionError on attacker")
-                desired = list(robot)
+        self.univector.set_target(g=(ball_x, ball_y), r=(ball_rx, ball_ry))
 
-        return desired
+        x, y = self.robot.x, self.robot.y
 
-    def get_virtual_obstacle(self, target):
-        '''
-        - m:    angle of the line perpendicular to the line between the ball and
-                the center of the goal
-        - p:    distance of the virtual obstacles to the ball / radius of the virtual obstacle
-        - vo:   virtual obstacle
-        - j:    this is the angle between the ball and the center of the goal
-        - m:    the normal angle perpendicular to j
-        - r:    radius of the ball
-        '''
-        aim_point = [self.field_w, self.field_h / 2]
+        theta_d = self.univector.compute((x, y))
+        theta_f = self.univector.compute(
+            (x + self.dl * math.cos(self.robot.theta),
+             y + self.dl * math.sin(self.robot.theta))
+        )
 
-        j = math.atan2(aim_point[1] - target[1], aim_point[0] - target[0])
-        m = j + math.pi / 2
-        p = 0.1
-
-        r = .0427 / 2
-
-        '''
-        the terms r*cos(j) and r*sin(j) are subtracted to move
-        the center of the obstacles behind the ball instead of its center
-        '''
-        if self.robot.y < math.tan(j) * (self.robot.x - target[0]) + target[1]:
-            virtual_obstacle = (
-                target[0] - p * math.cos(m) - r * math.cos(j),
-                target[1] - p * math.sin(m) - r * math.sin(j),
-                p,
-                1
-            )
-        else:
-            virtual_obstacle = (
-                target[0] + p * math.cos(m) - r * math.cos(j),
-                target[1] + p * math.sin(m) - r * math.sin(j),
-                p,
-                -1
-            )
-
-        if self.behind_ball():
-            virtual_obstacle = (
-                target[0] - p * math.cos(m) - r * math.cos(j),
-                target[1] - p * math.sin(m) - r * math.sin(j),
-                2 * p,
-                np.sign(target[1] - p * math.sin(m) - r * math.sin(j) - 0.65)
-            )
-
-        return virtual_obstacle
-
-    def behind_ball(self):
-        # Convert input to numpy arrays for easy calculation
-        point_on_line = np.array((self.match.ball.x, self.match.ball.y))
-        point_on_normal = np.array((1.5, .65))
-        point_to_check = np.array((self.robot.x, self.robot.y))
-
-        # Calculate the normal vector of the line
-        normal = point_on_normal - point_on_line
-
-        # Calculate the vector from the point on the line to the point to check
-        vector_to_check = point_to_check - point_on_line
-
-        # Calculate the dot product of the normal vector and the vector to check
-        dot_product = np.dot(normal, vector_to_check)
-
-        # Check the sign of the dot product to determine if the point is to the right or left of the line
-        return dot_product > 0
+        return theta_d, theta_f
 
 
 class WingPlay(PlayerPlay):
     def __init__(self, match, robot):
         super().__init__(match, robot)
+        self.dl = 0.000001
 
         self.BALL_Y_MIN = 0.05
         self.BALL_Y_MAX = 1.25
@@ -119,34 +58,34 @@ class WingPlay(PlayerPlay):
 
     def start_up(self):
         super().start_up()
-        controller = PID_W_control
+        controller = UniController
         self.robot.strategy.controller = controller(self.robot)
 
     def start(self):
-        self.limit_cycle = LimitCycle(self.match)
+        self.univector = UnivectorField(n=6, rect_size=.1)
         self.field_w, self.field_h = self.match.game.field.get_dimensions()
 
     def update(self):
         target = [self.match.ball.x, self.match.ball.y]
-        robot = (self.robot.x, self.robot.y)
 
         target[1] = min(self.BALL_Y_MAX, target[1])
         target[1] = max(self.BALL_Y_MIN, target[1])
 
-        aim_point = [self.field_w, self.BALL_Y_MAX] if target[1] > .65 else [self.field_w, self.BALL_Y_MIN]
+        ball_x, ball_y = target[0], target[1]
+        theta_ball = math.atan2(target[1] - ball_y, 1.6 - ball_x)
+        ball_rx, ball_ry = ball_x + .05 * math.cos(theta_ball), ball_y + .05 * math.sin(theta_ball)
 
-        if distance_between_points(target, robot) < 0.1 and not self.behind_ball(aim_point):
-            desired = aim_point
-        else:
-            self.limit_cycle.set_target(target)
-            self.limit_cycle.add_obstacle(self.get_virtual_obstacle(target))
-            try:
-                desired = self.limit_cycle.compute(self.robot, fitness=20)
-            except ZeroDivisionError:
-                print("ZeroDivisionError on attacker")
-                desired = list(robot)
+        self.univector.set_target(g=(ball_x, ball_y), r=(ball_rx, ball_ry))
 
-        return desired
+        x, y = self.robot.x, self.robot.y
+
+        theta_d = self.univector.compute((x, y))
+        theta_f = self.univector.compute(
+            (x + self.dl * math.cos(self.robot.theta),
+             y + self.dl * math.sin(self.robot.theta))
+        )
+
+        return theta_d, theta_f
 
     def get_virtual_obstacle(self, target):
         """
@@ -244,7 +183,7 @@ class Wait(PlayerPlay):
     def start_up(self):
         super().start_up()
         controller = PID_control
-        controller_kwargs={'V_MIN': 0, 'K_RHO': 75}
+        controller_kwargs={'V_MIN': 0, 'K_RHO': 1.5}
         self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
 
     def update(self):
@@ -344,4 +283,5 @@ class MainStriker(Strategy):
 
     def decide(self):
         res = self.playerbook.update()
+        print(self.playerbook.actual_play)
         return res
