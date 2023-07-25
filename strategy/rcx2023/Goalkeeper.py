fd9acd160db76aecff8ee7c69f9da4170eb96f10
Joao Pedro Leal <jpkleal@gmail.com>
2023-07-25 16:10:08 -0300

    feat(rcx 2023): add rest play for goalkeeper

--- strategy/rcx2023/Goalkeeper.py
+++ strategy/rcx2023/Goalkeeper.py
@@ -85,7 +85,6 @@ class InsideArea(PlayerPlay):
         )
 
         return ball.x, ball.y
-        return x+0.01*math.cos(theta_d), y+0.01*math.sin(theta_d)
 
 
 class Spin(PlayerPlay):
@@ -115,6 +114,32 @@ class Spin(PlayerPlay):
     def start(self):
         pass
 
+
+class Rest(PlayerPlay):
+    def __init__(self, match, robot):
+        super().__init__(match, robot)
+
+        self.target = (0.04, .65)
+    def get_name(self):
+        return f"<{self.robot.get_name()} Rest>"
+
+    def start_up(self):
+        super().start_up()
+        controller = PID_control
+        controller_kwargs = {
+            'K_RHO': 1,
+            'V_MIN': 0,
+        }
+        self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
+
+    def start(self):
+        pass
+
+    def update(self):
+
+        return self.target
+
+
 class Goalkeeper(Strategy):
     def __init__(self, match):
         super().__init__(match, "Goalkeeper_RSM2023", controller=PID_control)
@@ -133,20 +158,26 @@ class Goalkeeper(Strategy):
         spin = Spin(self.match, self.robot)
         spin.start()
 
+        rest = Rest(self.match, self.robot)
+        rest.start()
+
         self.playerbook.add_play(follow_ball)
         self.playerbook.add_play(inside_area)
         self.playerbook.add_play(spin)
+        self.playerbook.add_play(rest)
 
         on_near_ball = OnNextTo(self.match.ball, self.robot, 0.09)
         off_near_ball = OnNextTo(self.match.ball, self.robot, 0.12, True)
 
         follow_ball.add_transition(OnInsideBox(self.match, [-.5, .3, .65, .7]), inside_area)
         follow_ball.add_transition(on_near_ball, spin)
+        follow_ball.add_transition(OnInsideBox(self.match, [.75, -.3, 7, 1.9]), rest)
 
         inside_area.add_transition(OnInsideBox(self.match, [-.5, .3, .75, .8], True), follow_ball)
         inside_area.add_transition(on_near_ball, spin)
 
         spin.add_transition(off_near_ball, follow_ball)
+        rest.add_transition(OnInsideBox(self.match, [.75, -.3, 7, 1.9], True), follow_ball)
 
         if self.playerbook.actual_play == None:
             self.playerbook.set_play(follow_ball)
