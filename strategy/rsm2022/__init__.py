17510d3be0f466bbf7821d3a4eed51a147d03def
Joao Pedro Leal <jpkleal@gmail.com>
2023-08-23 18:01:46 -0300

    chore(strategies): remove old strategies

--- strategy/rsm2022/__init__.py
+++
@@ -1,6 +0,0 @@
-# Baseline de estrategias
-from strategy.rsm2022.Goalkeeper import Goalkeeper
-from strategy.rsm2022.Attacker import Attacker
-from strategy.rsm2022.Midfielder import Midfielder
-from strategy.rsm2022.uniuvf import UVF_Agent
-from strategy.rsm2022.uvf2 import UVFAgent2
