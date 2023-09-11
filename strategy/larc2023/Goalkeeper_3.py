43469aa29823c8fd3dc2be654a8ae76c35205381
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2023-09-11 15:05:42 -0300

    Add: Mandatory funcs from superclasses in Goalkeeper_3.py

---
+++ strategy/larc2023/Goalkeeper_3.py
@@ -0,0 +1,53 @@
+import math
+import numpy as np
+from strategy.BaseStrategy import Strategy
+from strategy.utils.player_playbook import PlayerPlay, PlayerPlaybook, OnInsideBox, AndTransition, OrTransition, \
+    NotTransition, OnNextTo
+from entities.plays.playbook import Trigger
+from controller import PID_control, PID_W_control, UniController
+from commons.math import point_in_rect
+from algorithms import UnivectorField
+
+class StayInArea(PlayerPlay):
+    def __init__(self, match, robot):
+        super().__init__(match, robot)
+    
+    def start_up(self):
+        super().start_up()
+    
+    def get_name(self):
+        return f"<{self.robot.get_name()} Stay in area>"
+    
+    def start_up(self):
+        super().start_up()
+        controller = PID_control
+        controller_kwargs = {
+            'K_RHO': 1,
+            'V_MIN': 0,
+        }
+        self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
+
+    def update(self):
+        ball = self.match.ball
+
+    
+
+
+class Goalkeeper (Strategy):
+    def __init__(self, match):
+        super().__init__(match, "Goalkeeper_RSM2023", controller=PID_control)
+
+    def start(self, robot=None):
+        super().start(robot=robot)
+
+        self.playerbook = PlayerPlaybook(self.match.coach, self.robot)
+
+    def reset(self, robot=None):
+        super().reset()
+        if robot:
+            self.start(robot)
+
+    def decide(self):
+        res = self.playerbook.update()
+        print(self.playerbook.actual_play)
+        return res
