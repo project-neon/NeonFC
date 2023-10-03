f574a5f8a05edab78a235d063a6042bc9b3bed21
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-03 18:41:24 -0300

    feat(replacer): add replacer declaration to strategy folder

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -2,6 +2,7 @@ from strategy import tests
 from strategy import rsm2023
 from strategy import rcx2023
 from strategy.BaseStrategy import Strategy
+from strategy import commons
 
 # debug tools
 from strategy import DebugTools
