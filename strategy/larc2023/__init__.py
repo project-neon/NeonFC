365acfa1d2b7be6ba7b61481ef4f76f239371c57
Raphael-AugustoG <giannattasio.raphael@aluno.ufabc.edu.br>
2023-10-04 16:53:41 -0300

    feat(larc2023): Organize files that were deleted.

--- strategy/larc2023/__init__.py
+++
@@ -1,6 +0,0 @@
-# Baseline de estrategias
-from strategy.larc2023.Goalkeeper_1 import Goalkeeper_Spin
-from strategy.larc2023.Goalkeeper_2 import Goalkeeper
-from strategy.larc2023.Attacker import MainStriker
-from strategy.larc2023.SecondAttacker import ShadowAttacker
-# from strategy.rcx2023.Unstuck import Unstuck
