3e4f8608c219d9914d0fdd34a943b00e45870cc4
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-05 16:14:04 -0300

    feat(replacer): make target position a class

--- strategy/commons/Replacer.py
+++ strategy/commons/Replacer.py
@@ -2,7 +2,7 @@ from algorithms.univector_field import UnivectorField
 import math
 from controller.PID_control import PID_control
 from strategy.BaseStrategy import Strategy
-from strategy.utils.player_playbook import PlayerPlaybook, PlayerPlay, OnNextTo, OnFoul
+from strategy.utils.player_playbook import PlayerPlaybook, PlayerPlay, OnNextTo
 from commons.math import distance_between_points
 
 # class Replacer(Strategy):
@@ -22,10 +22,41 @@ from commons.math import distance_between_points
 #
 #         return target
 
-class Position(PlayerPlay):
-    def __init__(self, match, robot, target_dict):
-        super().__init__(match, robot)
+class Target:
+    def __init__(self, target_dict):
+        self.x = 0
+        self.y = 0
         self.target_dict = target_dict
+        self.guide_x = 0
+        self.guide_y = 0
+
+    def update(self, foul, quadrant, mine):
+        if foul == 'KICKOFF':
+            target = self.target_dict[foul][mine]['robot']
+            guide = self.target_dict[foul][mine]['guide']
+        elif foul == 'FREE_BALL':
+            target = self.target_dict[foul][quadrant]['robot']
+            guide = self.target_dict[foul][quadrant]['guide']
+        elif foul == 'PENALTY_KICK':
+            target = self.target_dict[foul][mine]['robot']
+            guide = self.target_dict[foul][mine]['guide']
+        elif foul == 'GOAL_KICK':
+            target = self.target_dict[foul][mine]['robot']
+            guide = self.target_dict[foul][mine]['guide']
+        else:
+            target = [0, 0]
+            guide = [0, 0]
+
+        self.x = target[0]
+        self.y = target[1]
+
+        self.guide_x = guide[0]
+        self.guide_y = guide[1]
+
+class Position(PlayerPlay):
+    def __init__(self, match, robot, target):
+        super().__init__(match, robot)
+        self.target = target
 
     def get_name(self):
         return f"<{self.robot.get_name()} Position Planning>"
@@ -37,16 +68,15 @@ class Position(PlayerPlay):
         self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
 
     def update(self):
-
-        return self.target_dict
+        return self.target.x, self.target.y
 
     def start(self):
         pass
 
 class Angle(PlayerPlay):
-    def __init__(self, match, robot, angle):
+    def __init__(self, match, robot, target):
         super().__init__(match, robot)
-        self.target = angle
+        self.target = target
 
     def get_name(self):
         return f"<{self.robot.get_name()} Angle Planning>"
@@ -54,42 +84,39 @@ class Angle(PlayerPlay):
     def start_up(self):
         super().start_up()
         controller = PID_control
-        controller_kwargs = {'V_MIN': 0, 'V_MAX': 0, 'K_P': 100000}
+        controller_kwargs = {'V_MIN': 0, 'V_MAX': 0}
         self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
 
     def update(self):
-        return self.target
+        return self.target.guide_x, self.target.guide_y
 
     def start(self):
         pass
 
 
 class Replacer(Strategy):
-    def __init__(self, match, name="MainAttacker"):
+    def __init__(self, match,target_dict, name="MainAttacker"):
         super().__init__(match, name, controller=PID_control)
-
+        self.target = Target(target_dict)
         self.playerbook = None
 
     def start(self, robot=None):
         super().start(robot=robot)
 
-        field_dim = self.match.game.field.get_dimensions()
-
         # Criando Player Playbook: A maquina de estados do jogador
         self.playerbook = PlayerPlaybook(self.match.coach, self.robot)
 
         # Criando Path Planning baseado em Astar
-        target = [0.75, 0.65]
-        pos = Position(self.match, self.robot, target)
+        pos = Position(self.match, self.robot, self.target)
         pos.start()
-        angle = Angle(self.match, self.robot, [1.5, 0.65])
+        angle = Angle(self.match, self.robot, self.target)
         angle.start()
 
         self.playerbook.add_play(pos)
         self.playerbook.add_play(angle)
 
-        on_point = OnNextTo(target, self.robot, 0.02)
-        off_point = OnNextTo(target, self.robot, 0.02, True)
+        on_point = OnNextTo(self.target, self.robot, 0.02)
+        off_point = OnNextTo(self.target, self.robot, 0.02, True)
 
         pos.add_transition(on_point, angle)
         angle.add_transition(off_point, pos)
@@ -98,5 +125,8 @@ class Replacer(Strategy):
         self.playerbook.set_play(pos)
 
     def decide(self):
+        self.target.update(self.match.match_event['event'],
+                           self.match.match_event['quadrant'],
+                           str(self.match.match_event['mine']))
         res = self.playerbook.update()
         return res
