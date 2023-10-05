e31542cbc041214a5b212fabb31737addb0991d7
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-05 17:01:36 -0300

    feat(larc): create larc strategies

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,5 +1,6 @@
 from strategy import tests
 from strategy import rcx2023
+from strategy import larc2023
 from strategy.BaseStrategy import Strategy
 from strategy import commons
 
---
+++ strategy/larc2023/__init__.py
@@ -0,0 +1,5 @@
+# Baseline de estrategias
+from strategy.larc2023.Goalkeeper import Goalkeeper
+from strategy.larc2023.Attacker import MainStriker
+from strategy.larc2023.SecondAttacker import ShadowAttacker
+# from strategy.rcx2023.Unstuck import Unstuck
