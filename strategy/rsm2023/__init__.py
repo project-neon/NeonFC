8ddbb3aee7d552f43c3ab68120ae5926bcb67d57
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-05 17:01:00 -0300

    chore(rsm): remove old strategies and coach

--- strategy/rsm2023/__init__.py
+++
@@ -1,5 +0,0 @@
-# Baseline de estrategias
-from strategy.rsm2023.Goalkeeper import Goalkeeper
-from strategy.rsm2023.Attacker import MainStriker
-from strategy.rsm2023.SecondAttacker import ShadowAttacker
-from strategy.rsm2023.Unstuck import Unstuck
