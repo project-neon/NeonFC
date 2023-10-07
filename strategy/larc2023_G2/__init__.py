715c7b5cfd6ae465e27f7a140d84d5feddc86926
Raphael-AugustoG <giannattasio.raphael@aluno.ufabc.edu.br>
2023-10-06 23:25:38 -0300

    feat(real_life_larc2023):Organize branch to merge with branch larc_2023.

--- strategy/larc2023_G2/__init__.py
+++
@@ -1,5 +0,0 @@
-# Baseline de estrategias
-from strategy.larc2023_G2.Goalkeeper_2 import Goalkeeper
-from strategy.larc2023_G2.Attacker import MainStriker
-from strategy.larc2023_G2.SecondAttacker import ShadowAttacker
-# from strategy.rcx2023.Unstuck import Unstuck
