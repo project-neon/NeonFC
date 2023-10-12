ce95239559f785fdf16cabe2a1adc83791040794
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-12 14:02:10 -0300

    fix(goalkeeper): fix goalkeeper spinning in the wrong directions

--- strategy/rcx2023/Goalkeeper.py
+++ strategy/rcx2023/Goalkeeper.py
@@ -1,11 +1,7 @@
 import math
-import numpy as np
 from strategy.BaseStrategy import Strategy
-from strategy.utils.player_playbook import PlayerPlay, PlayerPlaybook, OnInsideBox, AndTransition, OrTransition, \
-    NotTransition, OnNextTo
-from entities.plays.playbook import Trigger
-from controller import PID_control, PID_W_control, UniController
-from commons.math import point_in_rect
+from strategy.utils.player_playbook import PlayerPlay, PlayerPlaybook, OnInsideBox, OnNextTo, AndTransition
+from controller import PID_control, PID_W_control, NoController
 from algorithms import UnivectorField
 
 
@@ -48,7 +44,7 @@ class FollowBallPlay(PlayerPlay):
 
         y = min(max(projection_point, self.goal_right), self.goal_left)
 
-        return 0.04, y
+        return 0.06, y
 
 
 class InsideArea(PlayerPlay):
@@ -66,23 +62,10 @@ class InsideArea(PlayerPlay):
         self.robot.strategy.controller = controller(self.robot, W_MAX=10)
 
     def start(self):
-        self.univector = UnivectorField(n=6, rect_size=.1)
-        self.field_w, self.field_h = self.match.game.field.get_dimensions()
+        pass
 
     def update(self):
         ball = self.match.ball
-        theta_ball = math.atan2(0.3 - ball.y, 0.15 - ball.x) if ball.y < self.robot.y else math.atan2(1 - ball.y, 0.15 - ball.x)
-        ball_rx, ball_ry = ball.x + .05 * math.cos(theta_ball), ball.y + .05 * math.sin(theta_ball)
-
-        self.univector.set_target(g=(ball.x, ball.y), r=(ball_rx, ball_ry))
-
-        x, y = self.robot.x, self.robot.y
-
-        theta_d = self.univector.compute((x, y))
-        theta_f = self.univector.compute(
-            (x + self.dl * math.cos(self.robot.theta),
-             y + self.dl * math.sin(self.robot.theta))
-        )
 
         return ball.x, ball.y
 
@@ -96,20 +79,16 @@ class Spin(PlayerPlay):
 
     def start_up(self):
         super().start_up()
-        controller = PID_W_control
-        controller_kwargs = {'V_MAX': 0, 'V_MIN': 0, 'W_MAX': 100000000000, 'KP':-10000000}
-        self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
+        controller = NoController
+        self.robot.strategy.controller = controller(self.robot)
 
     def update(self):
-        if self.robot.y > .65:
-            ang_diff = self.robot.theta - math.pi/2.1
+        if self.robot.y > self.match.ball.y:
+            w = 1_000
         else:
-            ang_diff = self.robot.theta + math.pi/2.1
+            w = -1_000
 
-        x = self.robot.x + 0.5*math.cos(ang_diff)
-        y = self.robot.y + 0.5*math.sin(ang_diff)
-
-        return x, y
+        return 0, w
 
     def start(self):
         pass
@@ -174,6 +153,11 @@ class Goalkeeper(Strategy):
         follow_ball.add_transition(OnInsideBox(self.match, [.75, -.3, 7, 1.9]), rest)
 
         inside_area.add_transition(OnInsideBox(self.match, [-.5, .3, .75, .8], True), follow_ball)
+        inside_area.add_transition(
+            AndTransition([
+                OnInsideBox(self.match, [-.5, .3, .65, .7], True),
+                off_near_ball])
+            , follow_ball)
         inside_area.add_transition(on_near_ball, spin)
 
         spin.add_transition(off_near_ball, follow_ball)
