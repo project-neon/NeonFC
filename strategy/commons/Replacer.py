c7d2344a7f656ac7f6b7bd46694baa9164365ff2
Joao Pedro Leal <jpkleal@gmail.com>
2023-10-12 13:54:09 -0300

    feat(replacer): create a logic to avoid other robots when replacing

--- strategy/commons/Replacer.py
+++ strategy/commons/Replacer.py
@@ -1,26 +1,8 @@
-from algorithms.univector_field import UnivectorField
-import math
-from controller.PID_control import PID_control
+from controller.PID_control import PID_control, PID_W_control
 from strategy.BaseStrategy import Strategy
 from strategy.utils.player_playbook import PlayerPlaybook, PlayerPlay, OnNextTo
-from commons.math import distance_between_points
+from algorithms.limit_cycle import LimitCycle
 
-# class Replacer(Strategy):
-#     def __init__(self, match, plot_field=False):
-#         super().__init__(match, "Replacer", controller=PID_control, controller_kwargs={'V_MIN': 0, 'K_RHO': 50})
-#
-#     def start(self, robot=None):
-#         super().start(robot=robot)
-#
-#     def reset(self, robot=None):
-#         super().reset()
-#         if robot:
-#             self.robot = robot
-#
-#     def decide(self):
-#         target = (.75, .65)
-#
-#         return target
 
 class Target:
     def __init__(self, target_dict):
@@ -53,6 +35,7 @@ class Target:
         self.guide_x = guide[0]
         self.guide_y = guide[1]
 
+
 class Position(PlayerPlay):
     def __init__(self, match, robot, target):
         super().__init__(match, robot)
@@ -64,7 +47,7 @@ class Position(PlayerPlay):
     def start_up(self):
         super().start_up()
         controller = PID_control
-        controller_kwargs={'V_MIN': 0, 'V_MAX':0.1 ,  'K_RHO': 25}
+        controller_kwargs={'V_MIN': 0, 'V_MAX':0.1 ,  'K_RHO': 6}
         self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
 
     def update(self):
@@ -72,6 +55,40 @@ class Position(PlayerPlay):
 
     def start(self):
         pass
+
+
+class Far(PlayerPlay):
+    def __init__(self, match, robot, target):
+        super().__init__(match, robot)
+        self.target = target
+
+    def get_name(self):
+        return f"<{self.robot.get_name()} Far Planning>"
+
+    def start_up(self):
+        super().start_up()
+        controller = PID_W_control
+        controller_kwargs={'V_MIN': 0, 'V_MAX':0.1}
+        self.robot.strategy.controller = controller(self.robot, **controller_kwargs)
+        self.limit_cycle = LimitCycle(self.match)
+
+    def update(self):
+        self.limit_cycle.set_target((self.target.x, self.target.y))
+        self.limit_cycle.del_all_obstacles()
+
+        for robot in self.match.robots:
+            self.limit_cycle.add_obstacle((robot.x, robot.y, 0.11, 0))
+
+        for robot in self.match.opposites:
+            self.limit_cycle.add_obstacle((robot.x, robot.y, 0.13, 0))
+
+        self.limit_cycle.add_obstacle((self.match.ball.x, self.match.ball.y, 0.13, 0))
+
+        return self.limit_cycle.compute(self.robot)
+
+    def start(self):
+        pass
+
 
 class Angle(PlayerPlay):
     def __init__(self, match, robot, target):
@@ -111,18 +128,22 @@ class Replacer(Strategy):
         pos.start()
         angle = Angle(self.match, self.robot, self.target)
         angle.start()
+        far = Far(self.match, self.robot, self.target)
 
         self.playerbook.add_play(pos)
         self.playerbook.add_play(angle)
+        self.playerbook.add_play(far)
 
         on_point = OnNextTo(self.target, self.robot, 0.02)
         off_point = OnNextTo(self.target, self.robot, 0.02, True)
+        on_near = OnNextTo(self.target, self.robot, 0.05)
 
         pos.add_transition(on_point, angle)
         angle.add_transition(off_point, pos)
+        far.add_transition(on_near, pos)
 
         # Estado inicial é o astar
-        self.playerbook.set_play(pos)
+        self.playerbook.set_play(far)
 
     def decide(self):
         self.target.update(self.match.match_event['event'],
