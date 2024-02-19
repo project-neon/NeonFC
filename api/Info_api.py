3773e8ac7494fd5f099f13974a21c2018574f65b
Raphael-AugustoG <giannattasio.raphael@aluno.ufabc.edu.br>
2024-02-19 08:42:04 -0300

    feat(api): Organizing new files before doing a rebase.

--- api/Info_api.py
+++ api/Info_api.py
@@ -14,13 +14,10 @@ class Info_Api():
 
         data_send = dict({
             'COACH_NAME' :  self.match.coach_name,
-            'TEAM_COLOR' :  self.match.team_color,
-            'CATEGORY' :    self.match.category,
             'TEAM_ROBOTS_POS' : [{f"{robot.robot_id}": (robot.x, robot.y, robot.theta)} for robot in self.robots],
             'OPPOSITE_ROBOTS_POS' : [{f"{robot.robot_id}": (robot.x, robot.y, robot.theta)} for robot in self.match.opposites],
-            'BALL_POS' : (self.ball.x, self.ball.y),
-            'GAME_STATUS' : self.match.game_status,
-            'TEAM_SIDE' : self.match.team_side
+            'ROBOTS_STRATEGY' : [{f"{robot.robot_id}": robot.strategy.name} for robot in self.robots],
+            'BALL_POS' : (self.ball.x, self.ball.y)
         })
 
         self.save_data(data_send)
