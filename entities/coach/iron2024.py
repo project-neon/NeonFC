4682c48aab7989d85429f55e9063d843d4e113d2
Joao Pedro Leal <jpkleal@gmail.com>
2024-02-23 15:27:59 -0300

    feat(iron2024): implement defenders on coach

--- entities/coach/iron2024.py
+++ entities/coach/iron2024.py
@@ -13,42 +13,59 @@ class Coach(BaseCoach):
         self.SS_strategy = strategy.iron2024.ShadowAttacker(self.match)
         self.ST_strategy = strategy.iron2024.MainStriker(self.match)
         self.GK_strategy = strategy.iron2024.Goalkeeper(self.match)
+        self.CB_strategy = strategy.iron2024.Defender(self.match)
+        self.SD_strategy = strategy.iron2024.ShadowDefender(self.match)
+
         self.GK_id = 8  # Goalkeeper fixed ID
+
+        self.defending = False
+
         positions = json.loads(open('foul_placements3v3.json', 'r').read())
-        self._position = [strategy.commons.Replacer(self.match, positions[str(r.robot_id)]) for r in self.match.robots]
+        self._position = {r.robot_id: strategy.commons.Replacer(self.match, positions[str(r.robot_id)]) for r in self.match.robots}
 
         # self.unstucks = {r.robot_id: strategy.rsm2023.Unstuck(self.match) for r in self.match.robots if r.robot_id != self.GK_id}
 
     def decide(self):
-        GK = [i for i, r in enumerate(self.match.robots) if r.robot_id is self.GK_id][0]
-        strikers = [r for i, r in enumerate(self.match.robots) if r.robot_id is not self.GK_id]
-        ST, SS = self.choose_main_striker(*strikers)
-
-        st_strat, ss_start = self.handle_stuck(ST, SS)
         if self.match.match_event['event'] == 'PLAYING':
+            GK = next(filter(lambda r: r.robot_id == self.GK_id, self.match.robots))
+            self.set_strategy(GK, self.GK_strategy)
 
-            if self.match.robots[GK].strategy != self.GK_strategy:
-                self.match.robots[GK].strategy = self.GK_strategy
-                self.match.robots[GK].start()
-
-            if ST.strategy != st_strat:
-                ST.strategy = st_strat
-                ST.start()
-
-            if SS.strategy != ss_start:
-                SS.strategy = ss_start
-                SS.start()
+            if self.match.ball.x < .4:
+                self.defend()
+            else:
+                self.attack()
 
         else:
-            robots = [(i, r.robot_id) for i, r in enumerate(self.match.robots)]
+            self.not_playing()
 
-            for robot, strategy in zip(robots, self._position):
-                if self.match.robots[robot[0]].strategy == strategy:
-                    # vamos evitar chamar o start todo frame
-                    # pode ser que essa strategia seja pesada de carregar
-                    continue
-                self.match.robots[robot[0]].strategy = strategy
-                self.match.robots[robot[0]].start()
+    def attack(self):
+        strikers = [r for i, r in enumerate(self.match.robots) if r.robot_id != self.GK_id]
+        ST, SS = self.choose_main_striker(*strikers)
+
+        self.set_strategy(ST, self.ST_strategy)
+        self.set_strategy(SS, self.SS_strategy)
+
+    def defend(self):
+        defenders = [r for i, r in enumerate(self.match.robots) if r.robot_id != self.GK_id]
+        CB, SD = self.choose_main_defender(*defenders)
+
+        print(CB.robot_id)
+
+        self.set_strategy(CB, self.CB_strategy)
+        self.set_strategy(SD, self.SD_strategy)
+
+    def not_playing(self):
+        robots = [(i, r.robot_id) for i, r in enumerate(self.match.robots)]
+        for robot, strategy in zip(robots, self._position):
+            if self.match.robots[robot[0]].strategy == strategy:
+                continue
+            self.match.robots[robot[0]].strategy = strategy
+            self.match.robots[robot[0]].start()
+
+    def set_strategy(self, robot, strat):
+        if robot != strat:
+            robot.strategy = strat
+            robot.start()
 
     def choose_main_striker(self, r1, r2):
         b = self.match.ball
@@ -63,6 +80,17 @@ class Coach(BaseCoach):
                 return r1, r2
             return r2, r1
         if b1 > 0:
+            return r1, r2
+        return r2, r1
+
+    def choose_main_defender(self, r1, r2):
+        b = self.match.ball
+
+        a1 = distance_between_points((b.x, b.y), (r1.x, r1.y))
+        a2 = distance_between_points((b.x, b.y), (r2.x, r2.y))
+
+
+        if a1 < a2:
             return r1, r2
         return r2, r1
 
