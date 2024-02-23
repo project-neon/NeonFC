d165194adc9fad58fa615baae278682ce435613d
Joao Pedro Leal <jpkleal@gmail.com>
2024-02-23 15:23:40 -0300

    feat(iron2024): create defender and secondary defender strategies

--- strategy/iron2024/__init__.py
+++ strategy/iron2024/__init__.py
@@ -2,4 +2,6 @@
 from strategy.iron2024.Goalkeeper import Goalkeeper
 from strategy.iron2024.Attacker import MainStriker
 from strategy.iron2024.SecondAttacker import ShadowAttacker
+from strategy.iron2024.Defender import Defender
+from strategy.iron2024.SecondDefender import ShadowDefender
 # from strategy.rcx2023.Unstuck import Unstuck
