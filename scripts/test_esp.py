fbd98598adc60701345e39b391461f34b42d7140
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-06-06 21:18:40 -0300

    feat/fix-firmare: esp test

--- scripts/test_esp.py
+++ scripts/test_esp.py
@@ -1,21 +1,32 @@
+import curses
 from comm import rl_comm
 
 c = rl_comm.RLComm()
 c.start()
 
-id = int(input("Qual o ID"))
+id = int(input("Qual o ID?\n"))
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
 
 def begin_test():
+
 	while True:
-		inp = input()
-		if inp == 'L':
-			print('lft')
-			c.send([{
-				'robot_id': id,
-				'color': 'yellow',
-				'wheel_left': 20,
-				'wheel_right': -20
-			}])
-		if inp == 'R':
-			print('rgh')
+		left, right = 0,0
+		
+		stdscr.refresh()
+		key = stdscr.getkey()
+
+		if key == 'KEY_LEFT':
+			left += 1
+		if key == 'KEY_RIGHT':
+			left += 1
+		
+
+		c.send([{
+			'robot_id': id,
+			'color': 'yellow',
+			'wheel_left': left,
+			'wheel_right': right
+		}])
 		print(inp)
