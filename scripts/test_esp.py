267ca252dec16fa9e9f5790d4cdd8810772308a5
mega2223 <juliocesarborgesdeoliveir@gmail.com>
2026-06-06 21:29:11 -0300

    feat/fix-firmare: esp test

--- scripts/test_esp.py
+++ scripts/test_esp.py
@@ -18,18 +18,18 @@ def begin_test():
 
 		if key == 'A':
 			stdscr.addstr('LEFT')
-			left += 1
+			left += .1
 		if key == 'D':
 			stdscr.addstr('RIGHT')
-			right += 1
+			right += .1
 		if key == 'W':
 			stdscr.addstr('FRONT')
-			left += 1
-			right += 1
+			left += .1
+			right += .1
 		if key == 'S':
 			stdscr.addstr('BACK')
-			left += 1
-			right += 1
+			left -= .1
+			right -= .1
 		
 		stdscr.addstr(f'ch = {key}\n')
 		stdscr.refresh()
